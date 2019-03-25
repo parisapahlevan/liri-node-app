813aa03cf88358c78b093aaf394cbe17aa2d0caa
Parisa Pahlevan <parisapahlevan@Zouhairs-MacBook-Pro-2.local>
2019-03-24 23:23:19 -0400

    final version

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # liri-node-app
 In this application, we will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
 
+
+1. Make an API call to "Bands in Town" and find your favorite bands perfomances: 
+Templet: node liri.js concert-this <artist/band name here>
+Example: node liri.js concert-this scorpions
+
+
+2. Make an API call to "Spotify" and look for your favorite songs. You will find 20 different version of of your favorite songs:
+Templet: node liri.js spotify-this-song '<song name here>' 
+Example: node liri.js spotify-this-song "bohemian rhapsody"
+
+
+3. Make an API call to "OMDB" and find your favorite movie's information:
+Templet: node liri.js movie-this '<movie name here>'
+Example: liri.js dumbo
+
+If the user doesn't type a movie in (node liri.js movie-this), the program will output data for the movie 'Mr. Nobody.'
+
+4. 
+
+
+
--- liri.js
+++ liri.js
@@ -1,59 +1,128 @@
 require("dotenv").config();
+readline = require('readline');
 var keys = require("./keys.js");
 var axios = require('axios');
-//var spotify = new Spotify(keys.spotify);
+var fs = require("fs")
+var Spotify = require('node-spotify-api');
 
+var spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
+});
 
 // FUNCTION DEFINITIONS ================================================================================
-function makeAPIcall(whatUserWant){
-    switch (whatUserWant) {
-        case "concert-this":
-            lookForConcert(process.argv[3])
-            break;
-        case "spotify-this-song":
-            lookForSong()
-            break;
-        case "movie-this":
-            lookForMovie()
-            break;
-        default:
-            console.log("I don't know what you want")
-            break;
+function makeAPIcall(whatUserWant) {
+  switch (whatUserWant) {
+    case "concert-this":
+      if(process.argv[3]){
+        lookForConcert(process.argv[3])
+      }else{
+        console.log("Please Provide the artist or the band name")
+      }
+      break;
+    case "spotify-this-song":
+      if(process.argv[3]){
+        lookForSong(process.argv[3])
+      }else{
+        console.log("Please Provide the song's name")
+      }
+      break;
+    case "movie-this":
+      lookForMovie(process.argv[3])
+      break;
+    default:
+      console.log("I don't know what you want")
+      break;
+  }
+}
+
+function lookForConcert(arg) {
+  var artist = arg.split("/")[0]
+  axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
+    .then(function (response) {
+      console.log("\n\n------------------ RESPONSE FROM API : -------------------")
+      console.log("Name of the venue: ", response.data[0].venue.name);
+      console.log("Venue location: ", response.data[0].venue.country + ", " + response.data[0].venue.city);
+      console.log("Date of the Event: ", fixMytime(response.data[0].datetime));
+      console.log("----------------------------------------------------------\n\n")
+    })
+    .catch(function (error) {
+      console.log("API ERROR: ", error);
+    })
+}
+
+function lookForSong(arg) {
+  spotify.search({ type: 'track', query: arg }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
     }
+    for (let i = 0; i < data.tracks.items.length; i++) {
+      console.log("Artist(s): ", data.tracks.items[i].artists[0].name)
+      console.log("The song's name: ", data.tracks.items[i].name)
+      console.log("A preview link of the song from Spotify: ", data.tracks.items[i].href)
+      console.log("The album that the song is from: ", data.tracks.items[i].album.name)
+      console.log("\n ----------------------------------------------")
+    }
+  });
 }
 
-
-function lookForConcert(arg){
-    var artist = arg.split("/")[0]
-    axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
-      .then(function (response) {
-        console.log("\n\n------------------ RESPONSE FROM API : -------------------")
-        console.log("Name of the venue: ", response.data[0].venue.name);
-        console.log("Venue location: ", response.data[0].venue.country + ", " + response.data[0].venue.city);
-        console.log("Date of the Event: ", fixMytime(response.data[0].datetime));
-        console.log("----------------------------------------------------------\n\n")
-      })
-      .catch(function (error) {
-        console.log("API ERROR: ", error);
-      });
-
+function lookForMovie(movie) {
+  if (!movie) {
+    movie = "Mr. Nobody"
+  }
+  axios.get(`http://www.omdbapi.com/?t=${movie}&apikey=203d1332`)
+    .then(function (response) {
+      console.log("\n\n------------------ RESPONSE FROM API : -------------------")
+      // console.log(response.data)
+      console.log("Title of the movie: ", response.data.Title);
+      console.log("Year the movie came out: ", response.data.Year);
+      console.log("IMDB Rating of the movie: ", response.data.imdbRating);
+      console.log("Rotten Tomatoes Rating of the movie: ", response.data.Ratings.Source);
+      console.log("Country where the movie was produced: ", response.data.Country);
+      console.log("Language of the movie: ", response.data.Language);
+      console.log("Plot of the movie: ", response.data.Plot);
+      console.log("Actors in the movie: ", response.data.Actors);
+      console.log("----------------------------------------------------------\n\n")
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
 }
 
-function lookForSong(){
-    console.log("get song")
-
+function fixMytime(str) {
+  var date = new Date(str);
+  return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
 }
 
-function lookForMovie(){
-    console.log("Get movie")
-
+async function readMyFile(fileName) {
+  const fileStream = fs.createReadStream(fileName);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  for await (const line of rl) {
+    var parseArr = line.split(',')
+    var apiToCall = parseArr[0];
+    console.log("\n\n\n\n\napiToCall: ", apiToCall)
+    var whatToLookFor = parseArr[1]
+    console.log("whatToLookFor: ", whatToLookFor)
+    if (apiToCall === "spotify-this-song") {
+      lookForSong(whatToLookFor)
+    }
+  }
 }
 
-function fixMytime(str){
-    var date = new Date(str);
-    return (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
+function liri(){
+  if(process.argv.length === 2){ // Means we read from the file if the user only entered: node liri.js 
+    readMyFile("random.txt")
+  }else{
+    makeAPIcall(process.argv[2])
+  }
 }
 
 // FUNCTION CALL================================================================================
 
-makeAPIcall(process.argv[2])
+liri()
+
+
+
